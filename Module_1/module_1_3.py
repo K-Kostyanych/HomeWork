afdc9d4d063c2a59fc1acecd16f9190fcb7d22f4
Костяныч <K.Kostyanych@yandex.ru>
2024-12-12 01:54:03 +0300

    HomeWork

--- Module_1/module_1_3.py
+++ Module_1/module_1_3.py
@@ -1,16 +1,6 @@
-
-
-name = 'Konstantin'
-print(type(name))
-age = 44
-print(type(age))
-new_age = (age) + 1
-print(new_age)
-is_student = True
-print(is_student)
-print('Name:' ,name)
-print('Age:' ,age)
-print('New age:' ,new_age)
-print('Is Student:' ,is_student)
-print()
-
+example = "Топинамбур"
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
--- fake_math.py
+++
@@ -1,5 +0,0 @@
-def fake_divide(first, second):
-    if second != 0:
-        return first / second
-    else:
-        print('Ошибка')
