be61a226cf9269aeb6570bec9f003a13daf611c9
Костяныч <K.Kostyanych@yandex.ru>
2024-12-11 10:52:25 +0300

    HomeWork

--- module_1_5.py
+++
@@ -1,9 +0,0 @@
-immutable_var = 3,7,'box', True
-print(immutable_var)
-immutable_var[0] = 5 #кортеж неизменяемый
-print(immutable_var)
-
-mutable_list = [3,5,7,8,9]
-mutable_list[0] = 'a'
-mutable_list[3] = 'c'
-print(mutable_list)
